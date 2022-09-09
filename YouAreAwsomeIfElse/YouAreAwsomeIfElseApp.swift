0c20c512b31a1c1585c19948a60f0473deeade80
SamarthAnnigeri <Samarthgrand@gmail.com>
2022-09-09 11:25:30 -0700

    Initial Commit

---
+++ YouAreAwsomeIfElse/YouAreAwsomeIfElseApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeIfElseApp.swift
+//  YouAreAwsomeIfElse
+//
+//  Created by Samarth Annigeri on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeIfElseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
